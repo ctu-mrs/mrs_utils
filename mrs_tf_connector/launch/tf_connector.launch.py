8c39b9f54d18d80014f0bba717626b138493dd7e
Afzal Ahmad <afzalhmd14@gmail.com>
2026-04-15 12:49:28 +0200

    fixed: no UAV_NAME namespace needed

--- mrs_tf_connector/launch/tf_connector.launch.py
+++ mrs_tf_connector/launch/tf_connector.launch.py
@@ -11,35 +11,24 @@ from launch.substitutions import LaunchConfiguration
 
 from ament_index_python.packages import get_package_share_directory
 
+
 def generate_launch_description():
 
     ld = launch.LaunchDescription()
 
-    pkg_name = 'mrs_tf_connector'
+    pkg_name = "mrs_tf_connector"
 
     this_pkg_path = get_package_share_directory(pkg_name)
-    namespace='tf_connector'
-
-    # #{ uav_name
-
-    uav_name = LaunchConfiguration('uav_name')
-
-    ld.add_action(DeclareLaunchArgument(
-        'uav_name',
-        default_value=os.getenv('UAV_NAME', 'uav1'),
-        description='The uav name used for namespacing.',
-    ))
-
-    # #} end of uav_name
+    namespace = "tf_connector"
 
     # #{ standalone
 
-    standalone = LaunchConfiguration('standalone')
+    standalone = LaunchConfiguration("standalone")
 
     declare_standalone = DeclareLaunchArgument(
-        'standalone',
-        default_value='true',
-        description='Whether to start a as a standalone or load into an existing container.'
+        "standalone",
+        default_value="true",
+        description="Whether to start a as a standalone or load into an existing container.",
     )
 
     ld.add_action(declare_standalone)
@@ -48,12 +37,12 @@ def generate_launch_description():
 
     # #{ container_name
 
-    container_name = LaunchConfiguration('container_name')
+    container_name = LaunchConfiguration("container_name")
 
     declare_container_name = DeclareLaunchArgument(
-        'container_name',
-        default_value='',
-        description='Name of an existing container to load into (if standalone is false)'
+        "container_name",
+        default_value="",
+        description="Name of an existing container to load into (if standalone is false)",
     )
 
     ld.add_action(declare_container_name)
@@ -62,51 +51,49 @@ def generate_launch_description():
 
     # #{ use_sim_time
 
-    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_sim_time = LaunchConfiguration("use_sim_time")
 
-    ld.add_action(DeclareLaunchArgument(
-        'use_sim_time',
-        default_value=os.getenv('USE_SIM_TIME', 'false'),
-        description='Should the node subscribe to sim time?',
-    ))
+    ld.add_action(
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value=os.getenv("USE_SIM_TIME", "false"),
+            description="Should the node subscribe to sim time?",
+        )
+    )
 
     # #} end of use_sim_time
 
     # #{ log_level
 
-    ld.add_action(DeclareLaunchArgument(name='log_level', default_value='info'))
+    ld.add_action(DeclareLaunchArgument(name="log_level", default_value="info"))
 
-    ld.add_action(DeclareLaunchArgument(name='topic_namespace', default_value=''))
+    ld.add_action(DeclareLaunchArgument(name="topic_namespace", default_value=""))
 
     # #} end of log_level
 
     # #{ default_node
 
     default_node = ComposableNode(
-
         package=pkg_name,
-        plugin=pkg_name+'::TFConnector',
-        namespace=uav_name,
+        plugin=pkg_name + "::TFConnector",
+        namespace="",
         name=namespace,
-
         parameters=[
-            {'uav_name': uav_name},
             {"use_sim_time": use_sim_time},
             {"public_config": this_pkg_path + "/config/tf_connector.yaml"},
         ],
-
         remappings=[
             # subscribers
-            ('~/tf_in', '/tf'),
+            ("~/tf_in", "/tf"),
             # publishers
-            ('~/tf_out', '/tf'),
+            ("~/tf_out", "/tf"),
         ],
     )
 
     load_into_existing = LoadComposableNodes(
         target_container=container_name,
         composable_node_descriptions=[default_node],
-        condition=UnlessCondition(standalone)
+        condition=UnlessCondition(standalone),
     )
 
     ld.add_action(load_into_existing)
@@ -116,15 +103,15 @@ def generate_launch_description():
     # #{ standalone container
 
     standalone_container = ComposableNodeContainer(
-        namespace=uav_name,
-        name=namespace+'_container',
-        package='rclcpp_components',
-        executable='component_container_mt',
+        namespace="",
+        name=namespace + "_container",
+        package="rclcpp_components",
+        executable="component_container_mt",
         output="screen",
-        arguments=['--ros-args', '--log-level', LaunchConfiguration('log_level')],
+        arguments=["--ros-args", "--log-level", LaunchConfiguration("log_level")],
         # prefix=['debug_roslaunch ' + os.ttyname(sys.stdout.fileno())],
         composable_node_descriptions=[default_node],
-        condition=IfCondition(standalone)
+        condition=IfCondition(standalone),
     )
 
     ld.add_action(standalone_container)
